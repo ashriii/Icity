b969a4ac4e3dd68e4a47fbcb399cd2d21bcce695
paribesh-koju <kojuparibesh1234@gmail.com>
2022-12-19 17:51:22 +0545

    putting the data of office

--- src/view/UserDashBoard.java
+++ src/view/UserDashBoard.java
@@ -5,6 +5,7 @@
 package view;
 import controller.TravellerController;
 import controller.HospitalController;
+import controller.OfficeController;
 import java.sql.*;
 
 import javax.swing.JOptionPane;
@@ -145,6 +146,11 @@ public class UserDashBoard extends javax.swing.JFrame {
             jButton7.setBackground(new java.awt.Color(153, 153, 153));
             jButton7.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
             jButton7.setText("Offices");
+            jButton7.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(java.awt.event.ActionEvent evt) {
+                    jButton7ActionPerformed(evt);
+                }
+            });
             getContentPane().add(jButton7, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 370, 120, 30));
 
             jButton8.setBackground(new java.awt.Color(153, 153, 153));
@@ -248,6 +254,33 @@ public class UserDashBoard extends javax.swing.JFrame {
             ex.printStackTrace();
         }
     }//GEN-LAST:event_jButton6ActionPerformed
+
+    private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton7ActionPerformed
+        OfficeController oc =new OfficeController();
+        ResultSet rs = oc.fetchOffice();
+        try {
+            ResultSetMetaData rsmd=rs.getMetaData();
+            DefaultTableModel model=(DefaultTableModel) jTable_Data_.getModel();
+            int colm=rsmd.getColumnCount();
+            String[] colmnName=new String[colm];
+            for(int i=0;i<colm;i++){
+                colmnName[i]=rsmd.getColumnName(i+1);
+                model.setColumnIdentifiers(colmnName);
+            }
+            
+            while(rs.next()){
+                   String name,description,address,contact;            
+                   name=rs.getString(1);
+                   description=rs.getString(2);
+                   address=rs.getString(3);
+                   contact=rs.getString(4);
+                   String[] row={name,description,address,contact};
+                   model.addRow(row);
+                }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }//GEN-LAST:event_jButton7ActionPerformed
 
     /**
      * @param args the command line arguments
